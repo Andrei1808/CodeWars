72d48038d5cac57aa1f13d2607eb74e07d6805df
Andrei <85843872+Andrei1808@users.noreply.github.com>
2021-11-07 00:04:00 +0200

    ddd

---
+++ 06.11.21.js
@@ -0,0 +1,58 @@
+function greet(name){
+    console.log("Hello, " + name + " how are you doing today?");
+}
+
+function makeNegative(num) {
+    if (num < 0){
+        return num;
+    }
+    else{
+        return -num;
+    }
+    // Code?
+}
+
+var summation = function (num) {
+
+    let result = 0;
+    for (var i = 1; i <= num; i++) {
+        result += i;
+    }
+
+    return result;
+}
+function isTriangle(a,b,c)
+{
+    if (a <= 0 || b <= 0 || c <= 0){
+        return false;}
+
+    else if (a+b > c && a+c > b && c+b > a)
+    { return true;}
+    else
+    {return false;}
+}
+
+function getMiddle(s)
+{if(s.length % 2 == 0){
+    return s.substring((s.length / 2) - 1 , (s.length / 2) + 1 );
+}
+else {
+    return s.substring(Math.floor(s.length / 2), Math.floor(s.length / 2) + 1);
+}
+}
+//Code goes here
+
+function array(arr){
+//Good luck
+    for (var x = 0; x < arr.length; x++);
+    if(arr.length > 3){
+        var y = arr.length - 1;
+        return arr.substring(1, y).replace(/[,]/g,' ').trim();
+
+    }
+    else {return null};
+    // str = $.trim(str);
+
+
+}
+
