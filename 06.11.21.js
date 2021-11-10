c0c64ff42772e0a14362b9b11a53426d0a2b25fc
Andrei <85843872+Andrei1808@users.noreply.github.com>
2021-11-10 21:01:38 +0200

    10.11.21

--- 06.11.21.js
+++ 06.11.21.js
@@ -56,3 +56,14 @@ function array(arr){
 
 }
 
+
+function hoopCount (n) {
+    if(n >= 10){
+        return "Great, now move on to tricks";
+    }
+    else{
+        return "Keep at it until you get it";
+    }
+    //your code goes here
+}
+
